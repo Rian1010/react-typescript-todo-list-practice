37e1f9710ad74eb7c29eb8f919271749231db5e5
Rian Sunthbocus <rian@Rians-MBP.local>
2021-09-05 18:39:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -44,3 +44,12 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+# Procedure
+
+## Start React Typescript App
+
+- `npm create react-app . --template typescript
+- Delete the logo, index.css, and both test files
+- Delete their imports and unneeded code
+- `npm start`
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
+import { JsxEmit, setTokenSourceMapRange } from 'typescript';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Type is equal to a type
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+// Interface creates a completely new type
+interface ITodo {
+  id: number;
+  text: string;
+  complete: boolean;
 }
+
+// Extend an interface
+// interface ITodo2 extends ITodo {
+//   tags: string[]
+// }
+
+const App = (): JSX.Element => {
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [edit, setEdit] = useState<boolean>(false);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  };
+
+  const addTodo = (text: string): void => {
+    let todoID = 0;
+    todoID++;
+    const newTodos: ITodo[] = [...todos, { id: todoID, text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
+    // [0,1,2,3].splice(2,1) = [0,1,3]
+    // [0,1,2,3].splice(2,2) = [0,1]
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (index: number) => {
+    const newTodos: ITodo[] = [...todos];
+
+    newTodos.map((t: ITodo) => {
+      if (t.id === newTodos[index].id) {
+        setEdit(!edit);
+      }
+    });
+
+    console.log(edit);
+  };
+
+  return (
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
+        <button type='submit'>Add Task</button>
+      </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <Fragment key={index}>
+            <div
+              key={index}
+              style={{ textDecoration: todo.complete ? 'line-through' : '' }}
+              onClick={() => updateTodo(index)}
+            >
+              {edit ? <input value={todo.text} /> : todo.text}
+            </div>
+            <button type='button' onClick={() => completeTodo(index)}>
+              {todo.complete ? 'Incomplete' : 'Complete'}
+            </button>
+            <button type='button' onClick={() => removeTodo(index)}>
+              x
+            </button>
+          </Fragment>
+        ))}
+      </section>
+    </Fragment>
+  );
+};
 
 export default App;
